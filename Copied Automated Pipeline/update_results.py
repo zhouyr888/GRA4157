f2a822d39a8a32c839c8a15550ab2491b291b6da
zhouyr888 <103897013+zhouyr888@users.noreply.github.com>
2024-10-11 01:03:48 +0200

    Add multiple inputs to copied automated pipeline file7

--- Copied Automated Pipeline/update_results.py
+++ Copied Automated Pipeline/update_results.py
@@ -3,11 +3,11 @@
 from main_code import f
 
 # Call the function f with input value 5
-d0, d1, d2 = f(5) # Unpacking the results
+d0, d1, d2 = f(29) # Unpacking the results
 
 # Write the results to a file
-with open("results_file.txt", "w") as file:
-    content = f"{d0}, {d1}, {d2}"
+with open("results_file.txt", "a") as file:
+    content = f"{d0}, {d1}, {d2}\n"
     file.write(content)
 
 print("Results saved to results_file.txt")
