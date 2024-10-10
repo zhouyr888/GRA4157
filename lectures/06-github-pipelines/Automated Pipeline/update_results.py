ff02783e6a04db2dfab0f71e041ba297c44bc2af
zhouyr888 <103897013+zhouyr888@users.noreply.github.com>
2024-10-10 20:49:40 +0200

    Switch to append mode for results_file.txt

--- lectures/06-github-pipelines/Automated Pipeline/update_results.py
+++ lectures/06-github-pipelines/Automated Pipeline/update_results.py
@@ -5,9 +5,9 @@ from main_code import f
 # Call the function f with input value 5
 d0, d1, d2 = f(10) # Unpacking the results
 
-# Write the results to a file
-with open("results_file.txt", "w") as file:
-    content = f"{d0}, {d1}, {d2}"
+
+with open("results_file.txt", "a") as file:  # Use 'a' for append mode
+    content = f"{d0}, {d1}, {d2}\n"  # Adding a newline for each new entry
     file.write(content)
 
 print("Results saved to results_file.txt")
